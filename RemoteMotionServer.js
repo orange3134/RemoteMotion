28287adbf2e4e91a15230f56dcf440b10eea500f
orange <mikan3134@gmail.com>
2021-10-21 22:09:37 +0900

    port

--- RemoteMotionServer.js
+++ RemoteMotionServer.js
@@ -1,6 +1,13 @@
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 // WebSocketのサーバの生成
 let ws = require('ws')
-var server = new ws.Server({port:5001});
+var server = new ws.Server({port:PORT});
 
 // 接続時に呼ばれる
 server.on('connection', ws => {
