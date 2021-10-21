4f0add2388e48f8c12e67d401405443a2967454d
orange <mikan3134@gmail.com>
2021-10-21 21:05:15 +0900

    First commit

---
+++ RemoteMotionServer.js
@@ -0,0 +1,23 @@
+// WebSocketのサーバの生成
+let ws = require('ws')
+var server = new ws.Server({port:5001});
+
+// 接続時に呼ばれる
+server.on('connection', ws => {
+    console.log('connected');
+    // クライアントからのデータ受信時に呼ばれる
+    ws.on('message', message => {
+        var messageString = message.toString();
+        console.log(messageString);
+
+        // クライアントにデータを返信
+        server.clients.forEach(client => {
+            client.send(messageString);
+        });
+    });
+
+    // 切断時に呼ばれる
+    ws.on('close', () => {
+        console.log('close');
+    });
+});
