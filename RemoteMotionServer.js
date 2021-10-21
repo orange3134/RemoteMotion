e162bcd0b4ffcd4dfab79a871007597eeafbf0f3
orange <mikan3134@gmail.com>
2021-10-21 21:27:09 +0900

    dployの準備

--- RemoteMotionServer.js
+++ RemoteMotionServer.js
@@ -4,7 +4,7 @@ var server = new ws.Server({port:5001});
 
 // 接続時に呼ばれる
 server.on('connection', ws => {
-    console.log('connected');
+    console.log('connected!');
     // クライアントからのデータ受信時に呼ばれる
     ws.on('message', message => {
         var messageString = message.toString();
